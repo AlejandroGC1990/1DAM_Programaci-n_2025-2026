ad2ea27a83f87d505a96693126f606c5fd2e060c
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-09-23 20:47:42 +0200

    eje 1.3

--- UT01_Introducción/ejercicios1.py
+++ UT01_Introducción/ejercicios1.py
@@ -31,3 +31,23 @@ print(suma)
 
 print("El producto del tercer y cuarto número es: ")
 print(producto)
+
+"""Realizar un programa que lea cuatro valores numéricos e imprima su suma y su promedio"""
+
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+num3 = float(input("Ingrese el tercer número: "))
+num4 = float(input("Ingrese el cuarto número: "))
+
+num1 = float(num1)
+num2 = float(num2)
+num3 = float(num3)
+num4 = float(num4)
+
+suma = num1 + num2 + num3 + num4
+promedio = suma /4
+
+print("La suma de los cuatro números es: ")
+print(suma)
+print("El promedio de los cuatro números es: ")
+print(promedio)
