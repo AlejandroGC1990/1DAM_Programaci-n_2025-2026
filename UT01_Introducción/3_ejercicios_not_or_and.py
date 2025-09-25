b11ec1e6e5637e5eb2dd660240fa26228d09380a
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-09-25 19:35:47 +0200

    ej not, or, and 3

--- UT01_Introducción/3_ejercicios_not_or_and.py
+++ UT01_Introducción/3_ejercicios_not_or_and.py
@@ -23,3 +23,32 @@ if num1 <= 10 and num2 <= 10 and num3 <= 10:
     print("Todos los números son menores a diez")
 else:
     print("Los números son " + num1 + "/" + num2 + "/" + num3)
+
+
+"""
+Se introducen por teclado tres números, si al menos uno de los valores introducidos es menor a 10, imprimir en
+pantalla la leyenda "Alguno de los números es menor a diez".
+"""
+num1 = int(input("Ingresar primer número"))
+num2 = int(input("Ingresar segundo número"))
+num3 = int(input("Ingresar tercer número"))
+
+if num1 <=10 or num2 <=10 or num3 <=10:
+    print("Alguno de los números es menor a diez")
+else:
+    print("Los números son " + num1 + "/" + num2 + "/" + num3)
+
+"""
+Se introducen tres valores por teclado, si todos son iguales se imprime la suma del primero con el segundo y a este
+resultado se le multiplica por el tercero.
+"""
+num1 = int(input("Ingresar primer número"))
+num2 = int(input("Ingresar segundo número"))
+num3 = int(input("Ingresar tercer número"))
+
+result = (num1 + num2) * num3
+
+if num1 == num2 and num1 == num3:
+    print("La suma de los 2 primeros números, multiplicando la suma por el num3, el resultado es " + result)
+else:
+    print("Los números son " + num1 + "/" + num2 + "/" + num3)
