290c6d1727d295dce7f7259636e55d747b9e27e9
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-09-25 19:11:40 +0200

    ej if 2

--- UT01_Introducción/2_ejercicios_if.py
+++ UT01_Introducción/2_ejercicios_if.py
@@ -2,6 +2,7 @@
 Realizar un programa que solicite la carga por teclado de dos números, si el primero es mayor al segundo imprimir su
 suma y diferencia, en caso contrario imprimir el producto y la división del primero respecto al segundo
 """
+
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
@@ -19,3 +20,29 @@ else:
     print(producto)
     print("División: ")
     print(division)
+
+"""
+Se introducen tres notas de un alumno, si el promedio es mayor o igual a cinco mostrar un mensaje "Aprobado".
+"""
+nota1 = float(input("Ingrese la primera nota: "))
+nota2 = float(input("Ingrese la segunda nota: "))
+nota3 = float(input("Ingrese la tercera nota: "))
+
+promedio = (nota1 + nota2 + nota3) / 3
+if promedio >= 5:
+    print("Aprobado")
+
+"""
+Se introduce por teclado un número positivo de uno o dos dígitos (1..99) mostrar un mensaje indicando si el número
+tiene uno o dos dígitos. (Tener en cuenta que condición debe cumplirse para tener dos dígitos un número entero)
+"""
+
+num = int(input("Ingrese un número positivo de uno o dos dígitos (1..99): "))
+if 1 <= num <= 9:
+    print("El número tiene un dígito.")
+else:
+    if 10 <= num <= 99:
+        print("El número tiene dos dígitos.")
+    else:
+        print("El número no es válido.")
+
