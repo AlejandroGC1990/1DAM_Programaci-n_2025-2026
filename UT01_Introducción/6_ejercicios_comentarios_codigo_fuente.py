f45838a0e49c8d2fd9a2b4615cf729a097b0e747
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-09-30 15:58:26 +0200

    ej 1 comentarios en codigo

---
+++ UT01_Introducción/6_ejercicios_comentarios_codigo_fuente.py
@@ -0,0 +1,10 @@
+"""
+Realizar un programa que solicite la carga de valores enteros por teclado y los sume. Finalizar la carga al introducir el
+valor -1. Dejar como comentario dentro del código fuente el enunciado del problema.
+"""
+
+"""
+Confeccionar un programa que solicite la carga de 10 valores reales por teclado. Mostrar al final su suma. Definir
+varias líneas de comentarios indicando el nombre del programa, el programador y la fecha de la última modificación.
+Utilizar el carácter # para los comentarios.
+"""
