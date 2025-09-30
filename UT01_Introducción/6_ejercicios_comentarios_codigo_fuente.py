0a167b6ed8302f8c6e57a281d0747cece0c48026
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-09-30 16:12:31 +0200

    ej 2 comentarios en codigo fuente

--- UT01_Introducción/6_ejercicios_comentarios_codigo_fuente.py
+++ UT01_Introducción/6_ejercicios_comentarios_codigo_fuente.py
@@ -12,7 +12,17 @@ while valor != -1:
 print("La suma de los valores ingresados es ", suma)
     
 """
-Confeccionar un programa que solicite la carga de 10 valores reales por teclado. Mostrar al final su suma. Definir
-varias líneas de comentarios indicando el nombre del programa, el programador y la fecha de la última modificación.
-Utilizar el carácter # para los comentarios.
+Confeccionar un programa que solicite la carga de 10 valores reales por 
+teclado. Mostrar al final su suma. Definir varias líneas de comentarios 
+indicando el nombre del programa, el programador y la fecha de la última 
+modificación. Utilizar el carácter # para los comentarios.
 """
+#Programa: Carga de 10 números
+#Programador: Alejandro García Carmona
+#Fecha de la ultima modificación.
+
+for x in range(10):
+    valor = float(input("Introduce el valor: "))
+    suma += valor
+
+print("La suma de los valores da: ", suma)
