0151437e2e0290f73f65c7f5890bd2dab0171ba0
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-09-30 16:03:11 +0200

    ej 1.1 comentarios en codigo

--- UT01_Introducción/6_ejercicios_comentarios_codigo_fuente.py
+++ UT01_Introducción/6_ejercicios_comentarios_codigo_fuente.py
@@ -2,7 +2,15 @@
 Realizar un programa que solicite la carga de valores enteros por teclado y los sume. Finalizar la carga al introducir el
 valor -1. Dejar como comentario dentro del código fuente el enunciado del problema.
 """
+suma = 0
+valor = int(input("Introduce el valor (con -1 termina): "))
 
+while valor != -1:
+    suma += valor
+    valor = int(input("Introduce el valor (con -1 termina): "))
+
+print("La suma de los valores ingresados es ", suma)
+    
 """
 Confeccionar un programa que solicite la carga de 10 valores reales por teclado. Mostrar al final su suma. Definir
 varias líneas de comentarios indicando el nombre del programa, el programador y la fecha de la última modificación.
