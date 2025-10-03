ffd868e99875cfdcd09e58f5ba1ddb71e8866b7e
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-03 18:01:23 +0200

    ejercicios extra - utilizando el formato f strings

---
+++ UT01_Introducción/EjerciciosExtra/2. Ejercicios utilizando el formato f strings.py
@@ -0,0 +1,22 @@
+#Ejercicios utilizando el formato f strings
+
+"""
+1. Escribir un programa en que pida al usuario el radio de un círculo. Calcular la
+longitud de la circunferencia y el radio del círculo y mostrar el resultado
+utilizando cadenas con formato (f-strings). Mostrar solo dos decimales
+"""
+"""
+2. Escribir un programa que pida al usuario una temperatura en grados Celsius. A
+continuación, convertir esa temperatura a grados Fahrenheit utilizando la
+fórmula C= 5/9(F-32) y mostrar el resultado.
+"""
+"""
+3. Escribir un programa en que muestre una tabla formateada de la siguiente
+manera: * El nombre en un campo de 10 caracteres (alineado a la izquierda). *
+La edad en un campo de 5 dígitos (alineado a la derecha). * El saldo en un
+campo de 12 dígitos (alineado a la derecha).
+                        Nombre  Edad  Saldo
+                        Juan     25  123.456
+                        Ana      30  245.987
+                        Carlos   22  567.123
+"""
