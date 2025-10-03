d29b0002ff21d8ec3e4a2f87832cf4f5b5b9422d
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-03 16:42:27 +0200

    ej 12 variables, condicionales y blucles

--- UT01_Introducción/Ejercicios de varialbes, condicionales y bucles.py
+++ UT01_Introducción/Ejercicios de varialbes, condicionales y bucles.py
@@ -116,6 +116,13 @@ print(not (15 > 20))
 12. Combina operadores aritméticos y lógicos: Verifica si el número resultante de la
 expresión (5 * 3) + 2 es mayor que 10 y menor que 20. Imprime el resultado.
 """
+operation = (5 * 3) + 2
+
+if operation > 10 and operation < 20:
+    print("El resultado está entre 10 y 20")
+else:
+    print("El resultado no está entre 10 y 20")
+    
 """
 13. Solicita al usuario que ingrese su edad y muestra un mensaje indicando si es
 mayor de edad (18 años o más) o menor de edad.
--- UT01_Introducción/pruebas.py
+++ UT01_Introducción/pruebas.py
@@ -1,5 +1,10 @@
 """
-11. Aplica el operador not para invertir el resultado de la comparación 15 > 20.
-¿Cuál es el resultado?
+12. Combina operadores aritméticos y lógicos: Verifica si el número resultante de la
+expresión (5 * 3) + 2 es mayor que 10 y menor que 20. Imprime el resultado.
 """
-print(not (15 > 20))
+operation = (5 * 3) + 2
+
+if operation > 10 and operation < 20:
+    print("El resultado está entre 10 y 20")
+else:
+    print("El resultado no está entre 10 y 20")
