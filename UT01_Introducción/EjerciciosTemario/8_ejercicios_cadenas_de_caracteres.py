329b6df7f2c35a1d7c09f467fce6b34ceffa7473
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-09 19:40:30 +0200

    ej 62 cadena de carácteres

--- UT01_Introducción/EjerciciosTemario/8_ejercicios_cadenas_de_caracteres.py
+++ UT01_Introducción/EjerciciosTemario/8_ejercicios_cadenas_de_caracteres.py
@@ -5,3 +5,5 @@ el primer carácter del nombre y la cantidad de letras que lo componen
 name = input("Introduce un nombre: ")
 
 print(f"El primer carácter del nombre es {name[0].capitalize()} y tiene {len(name)} letras")
+
+
