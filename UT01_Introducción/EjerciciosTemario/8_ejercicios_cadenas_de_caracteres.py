f5b4af2194b1e21b052e18525043e50e43d8205d
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-09 20:25:14 +0200

    ej 66 cadena de carácteres

--- UT01_Introducción/EjerciciosTemario/8_ejercicios_cadenas_de_caracteres.py
+++ UT01_Introducción/EjerciciosTemario/8_ejercicios_cadenas_de_caracteres.py
@@ -49,3 +49,13 @@ print(nombre2)
 print(nombre3)
 print(nombre4)
 
+"""
+eJ. 66: Cargar una oración por teclado. Mostrar cuantos espacios en blanco se introdujeron. Tener en cuenta que un espacio
+en blanco es igual a " ", en cambio una cadena vacía es ""
+
+"""
+oracion = input("Introduce una oración: ")
+count = 0
+x = 0
+
+print(oracion.count(" "))
---
+++ pruebas.py
@@ -0,0 +1,10 @@
+"""
+Cargar una oración por teclado. Mostrar cuantos espacios en blanco se introdujeron. Tener en cuenta que un espacio
+en blanco es igual a " ", en cambio una cadena vacía es ""
+"""
+oracion = input("Introduce una oración: ")
+count = 0
+x = 0
+
+print(oracion.count(" "))
+
