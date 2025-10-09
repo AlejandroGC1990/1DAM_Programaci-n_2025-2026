95f0a94feab7349254e88f0c0469a635f55d54c0
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-09 19:46:53 +0200

    ej 63 cadena de carácteres

--- UT01_Introducción/EjerciciosTemario/8_ejercicios_cadenas_de_caracteres.py
+++ UT01_Introducción/EjerciciosTemario/8_ejercicios_cadenas_de_caracteres.py
@@ -6,4 +6,13 @@ name = input("Introduce un nombre: ")
 
 print(f"El primer carácter del nombre es {name[0].capitalize()} y tiene {len(name)} letras")
 
+"""
+Ej. 63: Solicitar la carga del nombre de una persona en minúsculas. Mostrar un mensaje si 
+comienza con vocal dicho nombre.
+"""
+name = input("Introduce un nombre en minúsculas: ")
 
+if name[0] == "a" or name[0] == "e" or name[0] == "i" or name[0] == "o" or name[0] == "u":
+    print("El nombre introducido empieza con vocal")
+else:
+    print("El nombre introducido no empieza con vocal")
--- UT01_Introducción/pruebas.py
+++ UT01_Introducción/pruebas.py
@@ -1,7 +1,10 @@
 """
-Ej. 62: Realizar la carga del nombre de una persona y luego mostrar 
-el primer carácter del nombre y la cantidad de letras que lo componen
+Ej. 63: Solicitar la carga del nombre de una persona en minúsculas. Mostrar un mensaje si 
+comienza con vocal dicho nombre.
 """
-name = input("Introduce un nombre: ")
+name = input("Introduce un nombre en minúsculas: ")
 
-print(f"El primer carácter del nombre es {name[0].capitalize()} y tiene {len(name)} letras")
+if name[0] == "a" or name[0] == "e" or name[0] == "i" or name[0] == "o" or name[0] == "u":
+    print("El nombre introducido empieza con vocal")
+else:
+    print("El nombre introducido no empieza con vocal")
