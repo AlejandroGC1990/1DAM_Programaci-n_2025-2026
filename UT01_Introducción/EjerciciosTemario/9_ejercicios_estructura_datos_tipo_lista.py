62b7fd5efb9581f62b771436c8e92de68f5bbbd3
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-10 16:38:23 +0200

    ej 71 estructura de datos tipo lista

--- UT01_Introducción/EjerciciosTemario/9_ejercicios_estructura_datos_tipo_lista.py
+++ UT01_Introducción/EjerciciosTemario/9_ejercicios_estructura_datos_tipo_lista.py
@@ -20,3 +20,17 @@ listaMeses = ["enero", "febrero", "marzo", "abril"]
 
 print(listaMeses[0])
 print(listaMeses[-1])
+
+"""
+Ej. 71:  Definir una lista por asignación que almacene en el primer componente
+el nombre de un alumno y en los dos siguientes sus notas. Imprimir luego el
+nombre y el promedio de las dos notas.
+"""
+alumno = []
+name = input("Introduce el nombre del alumno: ")
+nota1 = float(input("Introduce la primera nota: "))
+nota2 = float(input("Introduce la segunda nota: "))
+promedio = (nota1 + nota2)/2
+alumno.extend([name, nota1, nota2])
+
+print(f"La nota promedio del alumno {alumno[0].capitalize()} y su nota promedia es de {promedio}")
--- pruebas.py
+++ pruebas.py
@@ -1,8 +1,13 @@
 """
-Ej. 70:  Definir una lista por asignación que almacene los nombres de los primeros
-cuatro meses de año. Mostrar el primer y último elemento de la lista solamente.
+Ej. 71:  Definir una lista por asignación que almacene en el primer componente
+el nombre de un alumno y en los dos siguientes sus notas. Imprimir luego el
+nombre y el promedio de las dos notas.
 """
-listaMeses = ["enero", "febrero", "marzo", "abril"]
+alumno = []
+name = input("Introduce el nombre del alumno: ")
+nota1 = float(input("Introduce la primera nota: "))
+nota2 = float(input("Introduce la segunda nota: "))
+promedio = (nota1 + nota2)/2
+alumno.extend([name, nota1, nota2])
 
-print(listaMeses[0])
-print(listaMeses[-1])
+print(f"La nota promedio del alumno {alumno[0].capitalize()} y su nota promedia es de {promedio}")
