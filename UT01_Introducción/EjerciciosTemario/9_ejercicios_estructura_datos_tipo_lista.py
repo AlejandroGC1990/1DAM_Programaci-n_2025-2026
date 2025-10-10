dc3c358022e13eeaaf4372bafb76dfdc86b63ff3
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-10 16:16:34 +0200

    ej 70 estructura de datos tipo lista

--- UT01_Introducción/EjerciciosTemario/9_ejercicios_estructura_datos_tipo_lista.py
+++ UT01_Introducción/EjerciciosTemario/9_ejercicios_estructura_datos_tipo_lista.py
@@ -11,3 +11,12 @@ while count < len(lista):
     count += 1
 
 print(f"Los elementos de la lista son {lista} y su suma total es {suma}")
+
+"""
+Ej. 70:  Definir una lista por asignación que almacene los nombres de los primeros
+cuatro meses de año. Mostrar el primer y último elemento de la lista solamente.
+"""
+listaMeses = ["enero", "febrero", "marzo", "abril"]
+
+print(listaMeses[0])
+print(listaMeses[-1])
--- pruebas.py
+++ pruebas.py
@@ -1,15 +1,8 @@
 """
-Ej. 69: Definir una lista que almacene 5 enteros. Sumar todos sus elementos
-y mostrar dicha suma.
+Ej. 70:  Definir una lista por asignación que almacene los nombres de los primeros
+cuatro meses de año. Mostrar el primer y último elemento de la lista solamente.
 """
-lista = [1, 2, 3, 4, 5]
-count = 0
-suma = 0
+listaMeses = ["enero", "febrero", "marzo", "abril"]
 
-while count < len(lista):
-    suma += lista[count]
-    count += 1
-
-print(f"Los elementos de la lista son {lista} y su suma total es {suma}")
-
-
+print(listaMeses[0])
+print(listaMeses[-1])
