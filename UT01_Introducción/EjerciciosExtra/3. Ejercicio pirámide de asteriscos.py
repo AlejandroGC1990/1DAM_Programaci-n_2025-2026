b395fd00a39f61f09de2ca2dc0e829928a585681
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-10 17:24:02 +0200

    ej extra. Pirámide de asteriscos

---
+++ UT01_Introducción/EjerciciosExtra/3. Ejercicio pirámide de asteriscos.py
@@ -0,0 +1,15 @@
+"""
+Hacer una pirámide de asteriscos con el número introducido por el usuario. Ej: 6.
+        *
+       ***
+      *****
+     *******
+    *********
+   ***********
+"""
+pisos = int(input("Introduce el número de pisos de la pirámide: "))
+asteriscos = 1
+for x in range (pisos):
+    print(f"{' ' * pisos}{'*' * asteriscos}")
+    asteriscos += 2
+    pisos -= 1
--- pruebas.py
+++ pruebas.py
@@ -1,13 +1,10 @@
 """
-Ej. 71:  Definir una lista por asignación que almacene en el primer componente
-el nombre de un alumno y en los dos siguientes sus notas. Imprimir luego el
-nombre y el promedio de las dos notas.
+Hacer una pirámide de asteriscos con el número introducido por el usuario
 """
-alumno = []
-name = input("Introduce el nombre del alumno: ")
-nota1 = float(input("Introduce la primera nota: "))
-nota2 = float(input("Introduce la segunda nota: "))
-promedio = (nota1 + nota2)/2
-alumno.extend([name, nota1, nota2])
+pisos = int(input("Introduce el númerode pisos de la pirámide: "))
+asteriscos = 1
+for x in range (pisos):
+    print(f"{' ' * pisos}{'*' * asteriscos}")
+    asteriscos += 2
+    pisos -= 1
 
-print(f"La nota promedio del alumno {alumno[0].capitalize()} y su nota promedia es de {promedio}")
