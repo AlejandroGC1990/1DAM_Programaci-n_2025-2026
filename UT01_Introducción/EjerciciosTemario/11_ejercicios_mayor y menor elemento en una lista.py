76aaff4b608ad269c43bb3e420a52b369d548002
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-16 20:59:41 +0200

    ej 81 mayor y menor elemento en una lista

--- UT01_Introducción/EjerciciosTemario/11_ejercicios_mayor y menor elemento en una lista.py
+++ UT01_Introducción/EjerciciosTemario/11_ejercicios_mayor y menor elemento en una lista.py
@@ -20,3 +20,27 @@ for y in range (1,5):
         mayor = lista[y]
 
 print(f"{mayor} es el mayor número de la lista {lista}")
+
+"""
+Ej. 81: Crear y cargar una lista con 5 enteros por teclado. Implementar un algoritmo que
+identifique el menor valor de la lista y la posición donde se encuentra
+"""
+lista= []
+
+for x in range (5):
+    while True:
+        try:
+            valor = int(input(f"Introduce el valor {x + 1}: "))
+            lista.append(valor)
+            break
+        except ValueError:
+            print("X Introduce un valor válido")
+
+menor = lista[0]
+
+for y in range (1,5):
+    if lista[y] < menor:
+        menor = lista[y]
+
+print(f"{menor} es el menor número de la lista {lista}")
+    
--- pruebas.py
+++ pruebas.py
@@ -1,8 +1,7 @@
 """
-Ej. 80: Crear y cargar una lista con 5 enteros. Implementar un algoritmo que identifique el
-mayor valor de la lista.
+Ej. 81: Crear y cargar una lista con 5 enteros por teclado. Implementar un algoritmo que
+identifique el menor valor de la lista y la posición donde se encuentra
 """
-
 lista= []
 
 for x in range (5):
@@ -14,11 +13,11 @@ for x in range (5):
         except ValueError:
             print("X Introduce un valor válido")
 
-mayor = lista[0]
+menor = lista[0]
 
 for y in range (1,5):
-    if lista[y] > mayor:
-        mayor = lista[y]
+    if lista[y] < menor:
+        menor = lista[y]
 
-print(f"{mayor} es el mayor número de la lista {lista}")
+print(f"{menor} es el menor número de la lista {lista}")
     
