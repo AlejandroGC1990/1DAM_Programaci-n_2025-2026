ce7cf80025be136def7252f96903004c082bdfea
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-16 20:26:03 +0200

    ej 78 carga de elementos para una lista

--- UT01_Introducción/EjerciciosTemario/10_ejercicios_carga de elementos por teclado para una lista.py
+++ UT01_Introducción/EjerciciosTemario/10_ejercicios_carga de elementos por teclado para una lista.py
@@ -60,3 +60,32 @@ promedioSueldos = sumaSueldos / len(sueldos)
 
 print(f"Los sueldos de los operarios son {sueldos} y el promedio de sueldos es {promedioSueldos}")
 
+"""
+Ej. 78: Cargar por teclado y almacenar en una lista las alturas de 5 personas (valores float)
+Obtener el promedio de las mismas. Contar cuántas personas son más altas que el promedio y
+cuántas más bajas.
+"""
+
+alturas = []
+sumaAlturas = 0
+alturasMpromedio = 0
+alturas_m_promedio = 0
+for x in range (5):
+    while True:
+        try:
+            altura = float(input(f"Introduce la altura {x + 1}"))
+            alturas.append(altura)
+            sumaAlturas += altura
+            break
+        except ValueError:
+            print("X Introduce un valor válido")
+
+promedioAlturas = sumaAlturas / len(alturas)
+
+for y in range (len(alturas)):
+    if y > promedioAlturas:
+        alturasMpromedio += 1
+    elif y < promedioAlturas:
+        alturas_m_promedio += 1
+
+print(f"El promedio de alturas es {promedioAlturas} y hay {alturasMpromedio} más altas que el promedio y {alturas_m_promedio} más bajas")
--- pruebas.py
+++ pruebas.py
@@ -1,22 +1,29 @@
 """
-Ej. 77: Almacenar en una lista los sueldos (valores float) de 5 operarios. Imprimir la
-lista y el promedio de sueldos.
+Ej. 78: Cargar por teclado y almacenar en una lista las alturas de 5 personas (valores float)
+Obtener el promedio de las mismas. Contar cuántas personas son más altas que el promedio y
+cuántas más bajas.
 """
 
-sueldos = []
-sumaSueldos = 0
-
+alturas = []
+sumaAlturas = 0
+alturasMpromedio = 0
+alturas_m_promedio = 0
 for x in range (5):
     while True:
         try:
-            sueldo = float(input(f"Introduce el sueldo del operario {x + 1}: "))
-            sueldos.append(sueldo)
-            sumaSueldos += sueldo
+            altura = float(input(f"Introduce la altura {x + 1}"))
+            alturas.append(altura)
+            sumaAlturas += altura
             break
         except ValueError:
             print("X Introduce un valor válido")
 
-promedioSueldos = sumaSueldos / len(sueldos)
+promedioAlturas = sumaAlturas / len(alturas)
 
-print(f"Los sueldos de los operarios son {sueldos} y el promedio de sueldos es {promedioSueldos}")
+for y in range (len(alturas)):
+    if y > promedioAlturas:
+        alturasMpromedio += 1
+    elif y < promedioAlturas:
+        alturas_m_promedio += 1
 
+print(f"El promedio de alturas es {promedioAlturas} y hay {alturasMpromedio} más altas que el promedio y {alturas_m_promedio} más bajas")
