b7af9b88cbc0f215a025f46e43068e4d78da6f39
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-16 20:11:37 +0200

    ej 77 carga de elementos para una lista

--- UT01_Introducción/EjerciciosTemario/10_ejercicios_carga de elementos por teclado para una lista.py
+++ UT01_Introducción/EjerciciosTemario/10_ejercicios_carga de elementos por teclado para una lista.py
@@ -37,3 +37,26 @@ while True:
         print("X Introduce un número entero")
         
 print(f"Los números son {nums}")
+
+"""
+Ej. 77: Almacenar en una lista los sueldos (valores float) de 5 operarios. Imprimir la
+lista y el promedio de sueldos.
+"""
+
+sueldos = []
+sumaSueldos = 0
+
+for x in range (5):
+    while True:
+        try:
+            sueldo = float(input(f"Introduce el sueldo del operario {x + 1}: "))
+            sueldos.append(sueldo)
+            sumaSueldos += sueldo
+            break
+        except ValueError:
+            print("X Introduce un valor válido")
+
+promedioSueldos = sumaSueldos / len(sueldos)
+
+print(f"Los sueldos de los operarios son {sueldos} y el promedio de sueldos es {promedioSueldos}")
+
--- pruebas.py
+++ pruebas.py
@@ -1,20 +1,22 @@
 """
-Ej. 76: Realizar la carga de valores enteros por teclado, almacenarlos en una lista.
-Finalizar la carga de enteros al introducir el cero. Mostrar finalmente el tamaño de la
-lista.
- 
+Ej. 77: Almacenar en una lista los sueldos (valores float) de 5 operarios. Imprimir la
+lista y el promedio de sueldos.
 """
 
-nums = []
+sueldos = []
+sumaSueldos = 0
 
-while True:
-    try:
-        n = int(input(f"Introduce números enteros (escribe '0' para finalizar la carga):"))
-        nums.append(n)
-
-        if n == 0:
+for x in range (5):
+    while True:
+        try:
+            sueldo = float(input(f"Introduce el sueldo del operario {x + 1}: "))
+            sueldos.append(sueldo)
+            sumaSueldos += sueldo
             break
-    except ValueError:
-        print("X Introduce un número entero")
-        
-print(f"Los números son {nums}")
+        except ValueError:
+            print("X Introduce un valor válido")
+
+promedioSueldos = sumaSueldos / len(sueldos)
+
+print(f"Los sueldos de los operarios son {sueldos} y el promedio de sueldos es {promedioSueldos}")
+
