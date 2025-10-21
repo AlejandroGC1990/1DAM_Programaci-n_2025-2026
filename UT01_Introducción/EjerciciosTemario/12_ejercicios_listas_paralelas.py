caab9bf7c20de15fd846bccee675b8966add7519
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-21 17:06:47 +0200

    ej 85 listas paralelas

--- UT01_Introducción/EjerciciosTemario/12_ejercicios_listas_paralelas.py
+++ UT01_Introducción/EjerciciosTemario/12_ejercicios_listas_paralelas.py
@@ -25,3 +25,30 @@ for y in range(5):
         mayores.append(listaName[y])
 
 print(f"Los mayores de edad son: {mayores}")
+
+"""
+Ej. 85: Crear y cargar dos listas con los nombres de 5 productos en una y sus respectivos
+precios en otra. Definir dos listas paralelas. Mostrar cuantos productos tienen un precio
+mayor al primer producto introducido.
+"""
+listaName = []
+listaPrice = []
+
+for x in range(5):
+    while True:
+        try:
+            name = input(f"Introduce el nombre {x + 1}: ")
+            listaName.append(name)
+            price = float(input(f"Introduce también el precio: "))
+            listaPrice.append(price)
+            break
+        except ValueError:
+            print("X Introduce un valor correcto")
+
+mayores = []
+
+for y in range(5):
+    if listaPrice[y] > listaPrice[0]:
+        mayores.append(listaName[y])
+
+print(f"Los productos más caros que el primero son: {mayores}")
