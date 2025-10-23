947cc064c157b1d34a176bc0936a35dea0ef367c
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-23 19:41:37 +0200

    ej 90 ordenamiento de elementos en listas

--- UT01_Introducción/EjerciciosTemario/13_ejercicios_ordenamiento_de_elementos_en_listas.py
+++ UT01_Introducción/EjerciciosTemario/13_ejercicios_ordenamiento_de_elementos_en_listas.py
@@ -52,3 +52,27 @@ for y in range(4):
 
 print(sueldos)
 
+"""
+Ej. 90: Crear una lista y almacenar los nombres de 5 países. Ordenar alfabéticamente la
+lista e imprimirla.
+"""
+
+paises = []
+
+for x in range(5):
+    while True:
+        try:
+            country = input(f"Introduce el país {x + 1}: ")
+            paises.append(country)
+            break
+        except ValueError:
+            print("X Introduce un valor correcto")
+
+for y in range(4):
+    for z in range(4):
+        if paises[z] > paises[z +1]:
+            aux = paises[z]
+            paises[z] = paises[z + 1]
+            paises[z + 1] = aux
+
+print(paises)
