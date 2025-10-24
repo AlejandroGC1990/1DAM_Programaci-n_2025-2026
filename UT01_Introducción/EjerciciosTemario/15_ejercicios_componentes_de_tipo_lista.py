8ee4a0a848144416a72af8ebe81ecf47ee31e8c5
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-24 17:47:43 +0200

    ej 98 ejercicios de componentes de tipo lista

--- UT01_Introducción/EjerciciosTemario/15_ejercicios_componentes_de_tipo_lista.py
+++ UT01_Introducción/EjerciciosTemario/15_ejercicios_componentes_de_tipo_lista.py
@@ -92,3 +92,21 @@ for z in range(len(listaN)):
 
 print(f"Lista de números: {listaN}")
 print(f"La suma total de los elementos es: {suma}")
+
+"""
+Ej. 98: Se tiene la siguiente lista:
+lista = [[100,7,85,8], [4,8,56,25], [67,89,23,1], [78,56]]
+Imprimir la lista. Después fijar con el valor cero todos los elementos mayores a 50 del
+primer elemento de "lista". Volver a imprimir la lista.
+"""
+
+lista = [[100,7,85,8], [4,8,56,25], [67,89,23,1], [78,56]]
+
+print(lista)
+
+for x in range(len(lista[0])):
+    if lista[0][x] > 50:
+        lista[0][x] = 0
+
+print(lista)
+
