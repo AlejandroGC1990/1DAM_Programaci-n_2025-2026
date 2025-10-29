346d616666f7914b006b6002254569febca0eeaa
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-29 20:30:48 +0100

    ej 109 ejercicios de eliminacion de componentes en una lista

--- Programacion/UT01_Introducción/EjerciciosTemario/17_ejercicios_listas_eliminacion_elementos.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/17_ejercicios_listas_eliminacion_elementos.py
@@ -22,3 +22,28 @@ lista.pop(2)
 
 print(f"Tras la eliminación: {lista}")
         
+"""
+Ej. 109: Crear una lista y almacenar 10 enteros pedidos por teclado. Eliminar todos los
+elementos que sean iguales al número entero 5. 
+"""
+lista = []
+listaSin5 = []
+
+for x in range(10):
+        while True:
+                try:
+                        num = int(input(f"Escribe el número {x + 1} de la lista: "))
+                        lista.append(num)
+                        break
+                except ValueError:
+                        print("X Introduce un valor correcto")
+
+
+print(f"Previa eliminación: {lista}")
+
+for y in range(len(lista)):
+        if lista[y] != 5:
+                listaSin5.append(lista[y])
+
+print(f"Tras la eliminación: {lista}")
+        
