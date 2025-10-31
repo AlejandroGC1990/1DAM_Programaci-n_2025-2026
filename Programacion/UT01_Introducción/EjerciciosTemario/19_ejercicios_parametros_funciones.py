96c36056ec2d9f5cc7efab2868ce038fae265bb9
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-31 16:50:24 +0100

    ej 117 ejercicios parametros de funciones

--- Programacion/UT01_Introducción/EjerciciosTemario/19_ejercicios_parametros_funciones.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/19_ejercicios_parametros_funciones.py
@@ -18,3 +18,23 @@ mostrarMensaje("Saludos")
 programa()
 mostrarMensaje("Adios")
 
+"""
+Ej. 117: Confeccionar una función que reciba tres enteros y nos muestre el mayor de ellos.
+La carga de los valores hacerlo por teclado.
+"""
+def mostrarValorMayor(v1, v2, v3):
+        if v1 > v2 and v1 > v3:
+                print(v1)
+        elif v2 > v3:
+                print(v2)
+        else:
+                print(v3)
+
+def cargarValores():
+        num1 = int(input("Introduce el primero número: "))
+        num2 = int(input("Introduce el segundo número: "))
+        num3 = int(input("Introduce el tercer número: "))
+
+        mostrarValorMayor(num1, num2, num3)
+
+cargarValores()
