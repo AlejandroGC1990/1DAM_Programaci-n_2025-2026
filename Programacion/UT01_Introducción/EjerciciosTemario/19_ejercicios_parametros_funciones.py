4160e41893112fce7d70ea8487ba85db0e783436
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-10-31 17:21:11 +0100

    ej 119 ejercicios parametros de funciones

--- Programacion/UT01_Introducción/EjerciciosTemario/19_ejercicios_parametros_funciones.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/19_ejercicios_parametros_funciones.py
@@ -62,3 +62,35 @@ def cargarDato():
 
 
 cargarDato()
+
+"""
+Ej. 119: Confeccionar una función que reciba tres enteros y los muestre ordenados de menor a
+mayor. En otra función solicitar la carga de 3 enteros por teclado y proceder a llamar a la
+primera función definida.
+"""
+def ordenarNums(v1, v2, v3):
+        if v1 < v2 and v1 < v3:
+                if(v2 < v3):
+                        print(v1, v2, v3)
+                else:
+                        print(v1, v3, v2)
+        else:
+                if(v2 < v3):
+                        if(v1 < v3):
+                                print(v2, v1, v3)
+                        else:
+                                print(v2, v3, v1)
+                else:
+                        if(v1 < v2):
+                                print(v3, v1, v2)
+                        else:
+                                print(v3, v2, v1)
+
+def cardarDatos():
+        num1 = int(input("Introduce el primer número entero: "))
+        num2 = int(input("Introduce el segundo número entero: "))
+        num3 = int(input("Introduce el tercer número entero: "))
+
+        ordenarNums(num1, num2, num3)
+
+cardarDatos()
