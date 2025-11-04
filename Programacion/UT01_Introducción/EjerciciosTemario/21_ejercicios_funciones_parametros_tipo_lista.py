91a0892ca22256af60cd33a551c6ed41d7eb553c
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-04 17:00:12 +0100

    ej 128 ejercicios funciones parametros de tipo lista

--- Programacion/UT01_Introducción/EjerciciosTemario/21_ejercicios_funciones_parametros_tipo_lista.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/21_ejercicios_funciones_parametros_tipo_lista.py
@@ -41,3 +41,37 @@ print("La suma de los valores de la lista es ", sumarLista(lista))
 print("El valor mayor de la lista es ", mayorLista(lista))
 print("El valor menor de la lista es ", menorLista(lista))
 
+"""
+Ej. 128: Crear y cargar por teclado en el bloque principal del programa una lista de 5
+enteros. Implementar una función que imprima el mayor y el menor valor de la lista
+"""
+def mayorMenor(lista):
+        result = []
+        mayor = lista[0]
+        menor = lista[0]
+
+        for x in range(1, len(lista)):
+                if lista[x] > mayor:
+                        mayor = lista[x]
+
+        result.append(mayor)
+
+        for y in range(1, len(lista)):
+                if lista[y]  < menor:
+                        menor = lista[y]
+
+        result.append(menor)
+
+        return result
+      
+
+lista = []
+
+for z in range(5):
+        num = int(input(f"Introduce el valor {z + 1} de la lista"))
+        lista.append(num)
+
+resultado = mayorMenor(lista)
+        
+print("El valor mayor de la lista es ", resultado[0])
+print("El valor menor de la lista es ", resultado[1])
