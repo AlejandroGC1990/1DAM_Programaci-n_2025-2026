8cb67e7a88dc950a28e378d1cf13401f09750e36
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-06 18:58:29 +0100

    ej 139 ejercicios funciones con parámetros con valor por defecto

--- Programacion/UT01_Introducción/EjerciciosTemario/23_ejercicios_funciones_parametros_con_valor_por_defecto.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/23_ejercicios_funciones_parametros_con_valor_por_defecto.py
@@ -10,3 +10,18 @@ def titulo_subrayado(titulo, caracter = "*"):
 
 titulo_subrayado("Sistema de Administración")
 titulo_subrayado("Ventas", "*")
+
+"""
+Ej. 139: Confeccionar una función que reciba entre 2 y 5 enteros. La misma nos debe retornar la
+suma de dichos valores. Debe tener tres parámetros por defecto. 
+"""
+def suma(v1, v2, v3 = 0, v4 = 0, v5 = 0):
+        s = v1 + v2 + v3 + v4 + v5
+        return s
+
+print("La suma de 5 + 6")
+print(suma(5,6))
+
+print("La suma de 1 + 2 + 3")
+print(suma(1,2, 3))
+
