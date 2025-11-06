58949724c3c03071ef5c4ed4a0098f05d05cbc48
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-06 19:44:55 +0100

    ej 144 ejercicios llamar función con argumentos nombrados

--- Programacion/UT01_Introducción/EjerciciosTemario/25_ejercicios_funciones_cantidad_variable_ de_parametros.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/25_ejercicios_funciones_cantidad_variable_ de_parametros.py
@@ -15,3 +15,23 @@ print("La suma de 1 + 2")
 print(suma(1, 2))
 print("La suma de 1 + 2 + 3 + 4 + 5")
 print(suma(1, 2, 3, 4, 5))
+
+"""
+Ej. 144: Confeccionar una función que reciba una serie de edades y me retorne la cantidad que
+son mayores o iguales a 18(como mínimo se envía un entero a la función)
+
+ 
+"""
+def mayor18(edad1, *edades):
+        count = 0
+
+        if edad1 >= 18:
+                count += 1
+
+        for x in range(len(edades)):
+                if edades[x] >= 18:
+                        count += 1
+
+        return count
+
+print("La cantidad de personas mayores de 18 son: ", mayor18(23, 6, 8, 19, 24))
