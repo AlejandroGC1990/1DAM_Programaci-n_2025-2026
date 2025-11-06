3f0634320b04d4280247fc1ced2df309d3f8e09d
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-06 20:26:27 +0100

    ej 150 estructura de datos tipo tupla

--- Programacion/UT01_Introducción/EjerciciosTemario/26_ejercicios_estructura_de_datos_tipo_tupla.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/26_ejercicios_estructura_de_datos_tipo_tupla.py
@@ -28,3 +28,32 @@ lista = cargar()
 mayor,menor = retornar_mayormenor(lista)
 print("Mayor valor de la lista: ", mayor)
 print("Menor valor de la lista: ", menor)
+
+"""
+Ej. 150: Confeccionar un programa con las siguientes funciones:
+1) Cargar el nombre de un empleado y su sueldo. Retornar una tupla con dichos valores
+2) Una función que reciba como parámetro dos tuplas con los nombres y sueldos de empleados y
+muestre el nombre del empleado con sueldo mayor.
+En el bloque principal del programa llamar dos veces a la función de carga y seguidamente llamar
+a la función que muestra el nombre de empleado con sueldo mayor.
+
+# bloque principal
+empleado1=cargar_empleado()
+empleado2=cargar_empleado()
+mayor_sueldo(empleado1,empleado2)
+"""
+def cargarEmpleado():
+        nombre = input("Introduce el nombre del empleado: ")
+        sueldo = float(input("Introduce un valor entero: "))
+
+        return (nombre,sueldo)
+
+def retornar_mayorSueldo(empleado1, empleado2):
+        if empleado1[1] > empleado2[1]:
+                print(empleado1[0], " tiene el sueldo mayor")
+        else:
+                print(empleado2[0], " tiene el sueldo mayor")
+                
+empleado1=cargarEmpleado()
+empleado2=cargarEmpleado()
+retornar_mayorSueldo(empleado1,empleado2)
