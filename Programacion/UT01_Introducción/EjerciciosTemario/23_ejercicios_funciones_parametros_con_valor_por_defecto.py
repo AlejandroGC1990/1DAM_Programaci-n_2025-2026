df1f9e8e57738fb4974fe72f684aa0dc27fc38d8
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-06 18:53:51 +0100

    ej 138 ejercicios funciones con parámetros con valor por defecto

---
+++ Programacion/UT01_Introducción/EjerciciosTemario/23_ejercicios_funciones_parametros_con_valor_por_defecto.py
@@ -0,0 +1,12 @@
+"""
+Ej. 138: Confeccionar una función que reciba un string como parámetro y en forma opcional un
+segundo string con un caracter. La función debe mostrar el string subrayado con el caracter que
+indica el segundo parámetro
+
+"""
+def titulo_subrayado(titulo, caracter = "*"):
+        print(titulo)
+        print(caracter * len(titulo))
+
+titulo_subrayado("Sistema de Administración")
+titulo_subrayado("Ventas", "*")
