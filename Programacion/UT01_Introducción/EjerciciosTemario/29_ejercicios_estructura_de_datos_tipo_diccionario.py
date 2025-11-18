9366da9935f8b98f8a398a62415829e534fdb2b5
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-18 16:07:46 +0100

    ej 160 estructura de datos tipo diccionario

--- Programacion/UT01_Introducción/EjerciciosTemario/29_ejercicios_estructura_de_datos_tipo_diccionario.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/29_ejercicios_estructura_de_datos_tipo_diccionario.py
@@ -9,3 +9,34 @@ def imprimir(paises):
 
 paises = {"argentina":40000000, "españa":460000000, "brasil":190000000, "uruguay":340000}
 imprimir(paises)
+
+"""
+Ej. 160: Crear un diccionario que permita almacenar 5 artículos, utilizar
+como clave el nombre de productos y como valor el precio del mismo.
+Desarrollar además las funciones de:
+1) Imprimir en forma completa el diccionario
+2) Imprimir solo los artículos con precio superior a 100. 
+"""
+def cargar():
+        productos = {}
+        for x in range(5):
+                nombre = input("Introduce el nombre del producto: ")
+                precio = int(input("Introduce el precio: "))
+                productos[nombre] = precio
+
+        return productos
+
+def imprimirProductos(productos):
+        print("Listado de todos los productos")
+        for nombre in productos:
+                print(nombre, productos[nombre])
+
+def imprimirMayor100(productos):
+        print("Listado de artículos con precio mayores a 100")
+        for nombre in productos:
+                if productos[nombre] > 100:
+                        print(nombre)
+
+productos = cargar()
+imprimir(productos)
+imprimirMayor100(productos)
