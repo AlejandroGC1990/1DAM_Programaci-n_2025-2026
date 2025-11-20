e1120553ad144af8bddc4d324f40901ef7b5a362
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-20 18:57:29 +0100

    ej 173 porciones de listas, tuplas y cadenas de carácteres

--- Programacion/UT01_Introducción/EjerciciosTemario/32_ejercicios_porciones_listas_tuplas_y_cadenas_de_caracteres.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/32_ejercicios_porciones_listas_tuplas_y_cadenas_de_caracteres.py
@@ -25,4 +25,31 @@ def primeros_tres(cadena):
 
 meses=('enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre')
 for x in meses:
- print(primeros_tres(x))
+    print(primeros_tres(x))
+
+"""
+Ej. 173: Realizar un programa que contenga las siguientes funciones:
+1) Carga de una lista de 10 enteros.
+2) Recibir una lista y retornar otra con la primer mitad (se sabe que siempre llega una
+lista con una cantidad par de elementos)
+3) Imprimir una lista.
+"""
+def cargar():
+        lista = []
+        for x in range(10):
+                valor = int(input("Cargar valor: "))
+                lista.append(valor)
+        return lista
+
+def retornar_mitad(lista):
+        mitad = len(lista)/2
+        return lista[:mitad]
+
+def imprimir(lista):
+        print("Contenido de la lista")
+        print(lista)
+
+lista = cargar()
+lista2 = retornar_mitad(lista)
+imprimir(lista)
+imprimir(lista2)
