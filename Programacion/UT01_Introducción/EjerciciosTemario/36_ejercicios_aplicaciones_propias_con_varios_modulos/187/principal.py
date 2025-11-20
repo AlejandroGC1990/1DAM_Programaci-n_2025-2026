7741fb63dae313e35a344dccfda099429259db04
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-20 20:33:51 +0100

    ej 187 aplicaciones propias con varios modulos

---
+++ Programacion/UT01_Introducción/EjerciciosTemario/36_ejercicios_aplicaciones_propias_con_varios_modulos/187/principal.py
@@ -0,0 +1,5 @@
+from mayormenor import mayor
+
+valor1 = int(input("Introduce primer valor: "))
+valor2 = int(input("Introduce segundo valor: "))
+print("El mayor de los dos valores es",mayor(valor1, valor2)) 
