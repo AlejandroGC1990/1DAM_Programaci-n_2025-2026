d0bf197ffa7e409db1d87d99ba299c502c16282d
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-20 19:39:29 +0100

    ej 180 biblioteca estándar de python

--- Programacion/UT01_Introducción/EjerciciosTemario/34_ejercicios_biblioteca_estandar_python.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/34_ejercicios_biblioteca_estandar_python.py
@@ -5,9 +5,6 @@ mostrar un mensaje con perdio.
 Para resolver este problema requerimos un algoritmo para que se genere un valor aleatorio
 entre 1 y 6. Como la generación de valores aleatorios es un tema muy frecuente, la biblioteca
 estándar de Python incluye un módulo que nos resuelve la generación de valores aleatorios.
-
-
-
 """
 import random
 
@@ -17,9 +14,39 @@ dado2 = random.randint(1, 6)
 print("Primer dado: ", dado1)
 print("Segundo dado: ", dado2)
 
-suma = dao1 + dado2
+suma = dado1 + dado2
 if suma == 7:
         print("Ganó")
 else:
         print("Perdió")
 
+"""
+Ej. 180: Desarrollar un programa que cargue una lista con 10 enteros.
+Cargar los valores aleatorios con números enteros comprendidos entre 0 y 1000.
+Mostrar la lista por pantalla.
+Luego mezclar los elementos de la lista y volver a mostrarlo
+
+
+
+"""
+import random
+
+def cargar():
+        lista = []
+        for x in range(10):
+                lista.append(random.randint(0, 1000))
+
+        return lista
+
+def imprimir(lista):
+        print(lista)
+
+def mezclar(lista):
+        random.shuffle(lista)
+
+lista = cargar()
+print("Lista generada aleatoriamente")
+imprimir(lista)
+mezclar(lista)
+print("La misma lista luego de mezclar")
+imprimir(lista)
