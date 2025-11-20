dbe243a2ec48360dcbb5474ec56780023b9804fe
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-20 20:11:14 +0100

    ej 185 importar funcionalidades de un modulo de la biblioteca estandar de python

--- Programacion/UT01_Introducción/EjerciciosTemario/35_ejercicios_importar_algunas_funcionalidades_de_un_modulo_de_la_biblioteca_estandar_de_python.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/35_ejercicios_importar_algunas_funcionalidades_de_un_modulo_de_la_biblioteca_estandar_de_python.py
@@ -12,3 +12,19 @@ r2 = pow(valor, 3)
 print("La raíz cuadrada es", r1)
 print("El cubo es", r2)
 
+"""
+Ej. 185: Calcular el factorial de un número introducido por teclado.
+El factorial de un número es la cantidad que resulta de la multiplicación de determinado
+número natural por todos los números naturales que le anteceden excluyendo el cero. Por
+ejemplo el factorial de 4 es 24, que resulta de multiplicar 4*3*2*1.
+No hay que implementar el algoritmo para calcular el factorial sino hay que importar dicha
+funcionalidad del módulo math.
+El módulo math tiene una función llamada factorial que recibe como parámetro un entero del
+que necesitamos que nos retorne el factorial.
+Solo importar la funcionalidad factorial del módulo math de la biblioteca estándar de Python
+"""
+from math import factorial
+
+valor = int(input("Introduce un valor: "))
+resu = factorial(valor)
+print("El factorial de ", valor, " es ", resu)
