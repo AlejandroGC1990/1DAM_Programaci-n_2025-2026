0b0690f2dc1c2e722f88c8314e0cca7ff92def44
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-20 18:53:37 +0100

    ej 172 porciones de listas, tuplas y cadenas de carácteres

--- Programacion/UT01_Introducción/EjerciciosTemario/32_ejercicios_porciones_listas_tuplas_y_cadenas_de_caracteres.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/32_ejercicios_porciones_listas_tuplas_y_cadenas_de_caracteres.py
@@ -13,3 +13,16 @@ print("Imprimir los nombres de meses que faltan hasta fin de año")
 numeromes = int("Introduce el número del mes: ")
 mesesfalta = meses_faltantes(numeromes)
 print(mesesfalta)
+
+"""
+Ej. 172: Confeccionar una función que reciba una cadena de caracteres y nos devuelva los tres
+primeros.
+En el bloque principal del programa definir una tupla con los nombres de meses. Mostrar por
+pantalla los primeros tres caracteres de cada mes
+"""
+def primeros_tres(cadena):
+        return cadena[:3]
+
+meses=('enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre')
+for x in meses:
+ print(primeros_tres(x))
