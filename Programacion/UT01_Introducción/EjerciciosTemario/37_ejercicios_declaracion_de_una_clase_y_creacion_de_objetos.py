31b28a54afb459cf2c20242a4b5c4e33d9070f61
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-21 16:41:59 +0100

    ej 189 declaracion_de_una_clase_y_creacion_de_objetos

--- Programacion/UT01_Introducción/EjerciciosTemario/37_ejercicios_declaracion_de_una_clase_y_creacion_de_objetos.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/37_ejercicios_declaracion_de_una_clase_y_creacion_de_objetos.py
@@ -67,3 +67,43 @@ persona1.imprimir()
 persona2 = Persona()
 persona2.inicializar("Carla", 18)
 persona2.imprimir()
+
+"""
+Ej. 189: Desarrollar un programa que cargue los lados de un triángulo e implemente los
+siguientes métodos: inicializar los atributos, imprimir el valor del lado mayor y otro método
+que muestre si es equilátero o no. El nombre de la clase llamarla Triangulo.
+
+"""
+class Triangulo:
+    def inicializar(self):
+        self.lado1 = int(input("Introduce el lado1 del triángulo: "))
+        self.lado2 = int(input("Introduce el lado2 del triángulo: "))
+        self.lado3 = int(input("Introduce el lado3 del triángulo: "))
+        
+    def imprimir(self):
+        print("Valores de los lados del triangulo: ")
+        print("Lado 1", self.lado1)
+        print("Lado 2", self.lado2)
+        print("Lado 3", self.lado3)
+      
+    def lado_mayor(self):
+        print("Lado mayor")
+        if self.lado1 > self.lado2 and self.lado1 > self.lado3:
+            print(self.lado1)
+        else:
+            if self.lado2 > self.lado3:
+                print(self.lado2)
+            else:
+                print(self.lado3)
+
+    def es_equilatero(self):
+         if self.lado1 == self.lado2 and self.lado1 == self.lado3:
+             print("El triangulo es equilatero")
+         else:
+             print("El triangulo no es equilatero")
+
+triangulo1 = Triangulo()
+triangulo1.inicializar()
+triangulo1.imprimir()
+triangulo1.lado_mayor()
+triangulo1.es_equilatero()
