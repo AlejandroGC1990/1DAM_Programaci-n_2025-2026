a522a1059529dc6026b7f40086cfa8637e94692c
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-27 17:36:27 +0100

    ejercicio HTML head

--- Programacion/pruebas.py
+++ Programacion/pruebas.py
@@ -1,34 +1,6 @@
 """
-Ej. 102: Se tiene que cargar la siguiente información:
- Nombres de 3 empleados
- Ingresos en concepto de sueldo, cobrado por cada empleado, en los últimos 3 meses.
- Confeccionar el programa para:
-a) Realizar la carga de los nombres de empleados y los tres sueldos por cada empleado.
-b) Generar una lista que contenga el ingreso acumulado en sueldos en los últimos 3 meses para cada empleado.
-c) Mostrar por pantalla el total pagado en sueldos a cada empleado en los últimos 3 meses
-d) Obtener el nombre del empleado que tuvo el mayor ingreso acumulado
+Ej. 190: Confeccionar una clase que represente un empleado. Definir como atributos su nombre
+y su sueldo. En el método __init__ cargar los atributos por teclado y luego en otro método
+imprimir sus datos y por último uno que imprima un mensaje si debe pagar impuestos (si el
+sueldo supera a 3000)
 """
- 
-listaNombres = []
-listaSueldos = []
-listaSueldosSumados = []
-
-for x in range(3):
-        nombre = input(f"Introduce el nombre del alumno nº{x + 1}: ")
-        listaNombres.append(nombre)
-
-        sublista = []
-        for y in range(2):
-            while True:
-                try:
-                    nota = float(input(f"Ingresa la nota {y + 1}: "))
-                    sublista.append(nota)
-                    break
-                except ValueError:
-                    print("X Introduce un valor válido para la nota")
-
-        listaNotas.append(sublista)
-                
-for z in range(len(listaNombres)):
-    print(f"{listaNombres[z]} tiene las notas: {listaNotas[z]}")
-                        
