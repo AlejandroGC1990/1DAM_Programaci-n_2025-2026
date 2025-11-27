f49aede45db13768d21f14ce8adffc24af21c085
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-27 19:38:58 +0100

    ejercicio 192 método __init__ de la clase

--- Programacion/UT01_Introducción/EjerciciosTemario/38_ejercicios_metodo_'_init_'_de_la_clase.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/38_ejercicios_metodo_'_init_'_de_la_clase.py
@@ -53,3 +53,25 @@ class Punto:
 
 punto1 = Punto()
 punto1.imprimir()
+
+"""
+Ej. 192: Desarrollar una clase que represente un Cuadrado y tenga los siguientes métodos:
+inicializar el valor del lado llegando como parámetro al método __init__ (definir un
+atributo llamado lado), imprimir su perímetro y su superficie.
+"""
+class Cuadrado:
+    def __init__(self, lado):
+        self.lado = lado
+
+    def mostrar_perimetro(self):
+        per = self.lado * 4
+        print("El períemtro del cuadrado es: ", per)
+
+    def mostrar_superficie(self):
+        sup = self.lado * self.lado
+        print("La superficie del cuadrado es: ", sup)
+
+  
+cuadrado1 = Cuadrado()
+cuadrado1.mostrar_perimetro()
+cuadrado1.mostrar_superficie()
