0a97a79d7fabe635ff7011cd6f4124049535970e
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-27 19:34:16 +0100

    ejercicio 191 método __init__ de la clase

--- Programacion/UT01_Introducción/EjerciciosTemario/38_ejercicios_metodo_'_init_'_de_la_clase.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/38_ejercicios_metodo_'_init_'_de_la_clase.py
@@ -23,3 +23,33 @@ class Empleado:
 empleado1 = Empleado()
 empleado1.imprimir()
 empleado1.paga_impuestos()
+
+"""
+Ej. 191: Desarrollar una clase que represente un punto en el plano y tenga los siguientes
+métodos: inicializar los valores de x e y que llegan como parámetros, imprimir en que
+cuadrante se encuentra dicho punto (concepto matemático, primer cuadrante si x e y son
+positivas, si x < 0 e y > 0 segundo cuadrante, etc.)
+"""
+class Punto:
+    def __init__(self):
+        self.x = float(input("Introduce el valor de x: "))
+        self.y = float(input("Introduce el valor de y: "))
+
+    def imprimir(self):
+        if self.x > 0 and self.y > 0:
+            print("Está en el cuadrante 1")
+        elif self.x > 0 and self.y < 0:
+            print("Está en el cuadrante 2")
+        elif self.x < 0 and self.y < 0:
+            print("Está en el cuadrante 3")
+        elif self.x > 0 and self.y < 0:
+            print("Está en el cuadrante 4")
+        elif self.x == 0 and self.y != 0:
+            print("Está sobre el eje Y")
+        elif self.y == 0 and self.x != 0:
+            print("Está sobre el eje X")
+        else:
+            print("Está en el origen (0,0)")
+
+punto1 = Punto()
+punto1.imprimir()
