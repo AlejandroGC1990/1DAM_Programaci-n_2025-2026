c6a6abe18e4304cf5cb10eb9feba8a6081e735dc
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-28 16:45:40 +0100

    ej 198 Colaboración de clases

--- Programacion/UT01_Introducción/EjerciciosTemario/40_ejercicios_colaboración_de_clases.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/40_ejercicios_colaboración_de_clases.py
@@ -61,3 +61,57 @@ class Banco:
 banco1 = Banco()
 banco1.operar()
 banco1.depositos_totales()
+
+"""
+Ej. 198: Plantear un programa que permita jugar a los dados. Las reglas de juego son:
+se tiran tres dados si los tres salen con el mismo valor mostrar un mensaje en el siga "gano",
+sino "perdió".
+Lo primero que hacemos es identificar las clases:
+Podemos identificar la clase Dado y la clase JuegoDeDados.
+Luego los atributos y los métodos de cada clase:
+Dado
+ atributos
+ valor
+ métodos
+ tirar
+ imprimir
+ retornar_valor
+goDeDados
+ atributos
+ 3 Dado (3 objetos de la clase Dado)
+ métodos
+ __init__
+ Jugar
+"""
+import random
+
+class Dado:
+    def tirar(self):
+        self.valor = random.randint(1,6)
+
+    def imprimir(self):
+        print("Valor del dado: ", self.valor)
+
+    def retornar_valor(self):
+        return self.valor
+
+class JuegoDeDados:
+    def __init__(self):
+        self.dado1 = Dado()
+        self.dado2 = Dado()
+        self.dado3 = Dado()
+
+    def jugar(self):
+        self.dado1.tirar()
+        self.dado1.imprimir()
+        self.dado2.tirar()
+        self.dado2.imprimir()
+        self.dado3.tirar()
+        self.dado3.imprimir()
+        if self.dado1.retornar_valor() == self.dado2.retornar_valor() and self.dado1.retornar_valor() == self.dado1.retornar_valor():
+            print("Gano")
+        else:
+            print("Perdió")
+
+juego_dados = JuegoDeDados()
+juego_dados.jugar()
