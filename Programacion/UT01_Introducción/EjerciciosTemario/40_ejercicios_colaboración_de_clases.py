659e3efdde92513825f94b2bab276ef745380a07
Alejandro G.C. <alejandrogc.dev@gmail.com>
2025-11-28 17:10:03 +0100

    ej 199 Colaboración de clases

--- Programacion/UT01_Introducción/EjerciciosTemario/40_ejercicios_colaboración_de_clases.py
+++ Programacion/UT01_Introducción/EjerciciosTemario/40_ejercicios_colaboración_de_clases.py
@@ -115,3 +115,41 @@ class JuegoDeDados:
 
 juego_dados = JuegoDeDados()
 juego_dados.jugar()
+
+"""
+Ej. 199: Plantear una clase Club y otra clase Socio.
+La clase Socio debe tener los siguientes atributos: nombre y la antigüedad en el club (en
+años). En el método __init__ de la clase Socio pedir la carga por teclado del nombre y su
+antigüedad. La clase Club debe tener como atributos 3 objetos de la clase Socio.
+Definir una responsabilidad para imprimir el nombre del socio con mayor antigüedad en el club.
+"""
+class Socio:
+    def __init__(self):
+        self.nombre = input("Introduce el nombre del socio: ")
+        self.antiguedad = int(input("Introduce la antigüedad: "))
+
+    def imprimir(self):
+        print(self.nombre, " tiene una antigüedad de ", self.antiguedad)
+
+    def retornar_antiguedad(self):
+        return self.antiguedad
+
+class Club:
+    def __init__(self):
+        self.socio1 = Socio()
+        self.socio2 = Socio()
+        self.socio3 = Socio()
+
+    def mayor_antiguedad(self):
+        print("Socio con mayor antigüedad")
+        if (self.socio1.retornar_antiguedad() > self.socio2.retornar_antiguedad() and
+            self.socio1.retornar_antiguedad() > self.socio3.retornar_antiguedad()):
+            self.socio1.imprimir()
+        else:
+            if self.socio2.retornar_antiguedad() > self.socio3.retornar_antiguedad():
+                self.socio2.imprimir()
+            else:
+                self.socio3.imprimir()
+
+club = Club()
+club.mayor_antiguedad()
